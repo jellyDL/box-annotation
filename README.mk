f2dd968b43d285ee3cb50046d4006e8b263bbbcc
hzwangjialei <hzwangjialei@corp.netease.com>
2018-04-12 10:45:32 +0800

    update README.mk

--- README.mk
+++ README.mk
@@ -8,12 +8,16 @@ pip install labelme  # both python2/3 should work
 ```
 官方Git：[LableMe](git:https://github.com/xiaoxiongguilai/box-annotation)
 
-标注后，生成每个图片的json数据文件
+将需要标注图像放到idcard_annotions_xxx下
+./labelme  使用labelme标注数据，标注后生成json文件，与对应jpg都会放在同一文件夹idcard_annotions_xxx下
 
 ### 2.根据json文件，生成GroundTruth图片
 
 详见 __create_gt_image.ipynb__
 
+
+## 训练数据版本：
+20180403: 选取了FrontImages_train 的前500个图像作为标注数据
 
 
 
